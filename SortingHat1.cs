a6adab8f0e338ae6f957725416e862bec30f4b0b
Alex Bulankou <bulankou@gmail.com>
2017-04-03 09:28:00 -0700

    Add files via upload

---
+++ SortingHat1.cs
@@ -0,0 +1,83 @@
+﻿namespace SortingHat1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public interface IItemWithKey<TKey>
+    {
+        TKey Key { get; set; }
+    }
+
+    public interface IEntryLookup<TKey>
+    {
+        IItemWithKey<TKey> GetEntryByKey(TKey key);
+
+    }
+
+    public class EntryLookupList<TKey> : IEntryLookup<TKey>
+           where TKey : IEquatable<TKey>
+    {
+        private List<IItemWithKey<TKey>> entries;
+        public EntryLookupList(IEnumerable<IItemWithKey<TKey>> entries)
+        {
+            this.entries = new List<IItemWithKey<TKey>>(entries);
+        }
+
+        public IItemWithKey<TKey> GetEntryByKey(TKey key)
+        {
+            return entries.First(item => item.Key.Equals(key));
+        }
+    }
+
+    public class EntryLookupBinarySearch<TKey, TItem> : IEntryLookup<TKey>, IComparer<TItem>
+           where TKey : IEquatable<TKey>, IComparable<TKey>
+           where TItem : IItemWithKey<TKey>, new()
+    {
+        private List<TItem> entries;
+        public EntryLookupBinarySearch(IEnumerable<TItem> entries)
+        {
+            this.entries = new List<TItem>(entries);
+        }
+
+        public int Compare(TItem x, TItem y)
+        {
+            return x.Key.CompareTo(y.Key);
+        }
+
+        public IItemWithKey<TKey> GetEntryByKey(TKey key)
+        {
+            var entryToSearch = new TItem();
+            entryToSearch.Key = key;
+            var index = entries.BinarySearch(entryToSearch, this);
+            return this.entries[index];
+
+        }
+    }
+
+    public class EntryLookupDictionary<TKey> : IEntryLookup<TKey>, IEqualityComparer<TKey>
+           where TKey : IEquatable<TKey>
+    {
+        private Dictionary<TKey, IItemWithKey<TKey>> entryDictionary;
+
+        public EntryLookupDictionary(IEnumerable<IItemWithKey<TKey>> entries)
+        {
+            this.entryDictionary = entries.ToDictionary(entry => entry.Key, this);
+        }
+
+        public IItemWithKey<TKey> GetEntryByKey(TKey key)
+        {
+            return this.entryDictionary[key];
+        }
+
+        public bool Equals(TKey x, TKey y)
+        {
+            return x.Equals(y);
+        }
+
+        public int GetHashCode(TKey obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
